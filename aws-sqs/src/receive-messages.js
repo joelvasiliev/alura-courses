a7a6b7b19625a60d6639c0d8a27add61bddc4af8
joel.neto <joel@LP1432.inmetrics.local>
2022-03-07 13:58:45 -0300

    adicionado php-primeiros-passos

--- aws-sqs/src/receive-messages.js
+++ aws-sqs/src/receive-messages.js
@@ -1,9 +1,9 @@
 const AWS = require('aws-sdk')
 AWS.config.update({
-    region: 'us-east-1'
+    region: 'sa-east-1'
 })
 const sqs = new AWS.SQS();
-const QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/364350264218/alura-test';
+const QUEUE_URL = 'https://sqs.sa-east-1.amazonaws.com/483512691173/teste-001.fifo';
 
 (async () => {
     const messages = await sqs.receiveMessage({
@@ -12,13 +12,14 @@ const QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/364350264218/alura-test';
         WaitTimeSeconds: 20
     }).promise();
 
+    console.log(messages)
+
     if (messages.Messages) {
         messages.Messages.forEach(async message => {
             try {
                 const body = JSON.parse(message.Body);
                 console.log('processando mensagem...');
-                console.log(`realizando transferência da conta ${body.conta_origem.numero_conta} para a conta ${body.conta_destino.numero_conta}`);
-                console.log(`transferência no valor de ${body.valor} ${body.moeda} efetivada!`);
+                console.log(`mensagem encontrada do chatid: ${body.chatid}`);
                 await sqs.deleteMessage({
                     QueueUrl: QUEUE_URL,
                     ReceiptHandle: message.ReceiptHandle
---
+++ php-primeiros-passos/desafios/imc.php
@@ -0,0 +1,19 @@
+<?php
+
+$peso = 83;
+$altura = 1.80;
+$imc = $peso / $altura ** 2;
+
+echo "Seu IMC é de $imc. Você está com o IMC ";
+
+if ($imc < 18.5){
+    echo "abaixo";
+}
+else if ($imc < 25){
+    echo "dentro";
+}
+else{
+    echo "acima";
+}
+
+echo " do recomendado.\n";
